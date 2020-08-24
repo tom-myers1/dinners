c1cf1ef626d370582e25805818f59b95afab63f7
Tom Myers <tom.myers@zolv.com>
2020-08-24 16:57:46 +0100

    updated data structure for recipes

--- dinners/dinners/Data/Product.cs
+++ dinners/dinners/Data/Product.cs
@@ -14,5 +14,62 @@ namespace dinners.Data
         public int Quantity { get; set; }
     }
 
+    public class Catagories
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
 
+    public class Catagory
+    {
+        public int RecipeId { get; set; }
+        public int CatagoryId { get; set; }
+    }
+
+    public class Ingredient
+    {
+        public int RecipeId { get; set; }
+        public int Line { get; set; }
+        public int AmountInt { get; set; }
+        public int AmountNum { get; set; }
+        public int AmountDenom { get; set; }
+        public float Amountfloat { get; set; }
+        public string Unit { get; set; }
+        public int IngredientId { get; set; }
+    }
+
+    public class Ingredients
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class IngredientsSelection
+    {
+        public int RecipeId { get; set; }
+        public int Line { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class Instruction
+    {
+        public int RecipeId { get; set; }
+        public int Line { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class InstructionSection
+    {
+        public int RecipeId { get; set; }
+        public int Line { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class Recipes
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Servings { get; set; }
+        public string ServingUnit { get; set; }
+    }
 }
